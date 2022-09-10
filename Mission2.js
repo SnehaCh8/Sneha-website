a672fbc8af7e61db7febb35cf18cd269f5215928
Sneha Choudhary <snehac@missionreadyhq.com>
2022-09-10 21:26:02 +0530

    new

--- Mission2.js
+++ Mission2.js
@@ -1,4 +1,21 @@
-/*let change = document.getElementById('changeimg');
-change.addEventListener('click', function (change){
-    change.target.classlist.toggle('bImage2');
-})*/
+let img = document.querySelector('img');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+
+btn1.addEventListener('click', () => {
+    img.src = 'eng1.png';
+})
+
+btn2.addEventListener('click', () => {
+    img.src = 'eng2.png';
+})
+
+btn3.addEventListener('click', () => {
+    img.src = 'eng3.png';
+})
+
+btn4.addEventListener('click', () => {
+    img.src = 'eng4.png';
+})
